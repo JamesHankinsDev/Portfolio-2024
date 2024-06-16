f656f935537c73e3d0cdcc4056d47b2acf4299e7
JHankins09 <Hankins.JamesB@gmail.com>
2024-06-15 22:36:47 -0400

    Update contact me modal

--- src/app/components/ContactMe.tsx
+++ src/app/components/ContactMe.tsx
@@ -8,6 +8,8 @@ import {
   ModalBody,
   ModalFooter,
   Button,
+  Input,
+  Textarea,
 } from '@nextui-org/react';
 
 import { useDisclosure } from '@nextui-org/react';
@@ -19,12 +21,23 @@ export default function ContactMe() {
     onOpen();
   };
 
+  const [subject, setSubject] = React.useState<string | null>(null);
+  const [body, setBody] = React.useState<string | null>(null);
+  const [sentEmail, setSentEmail] = React.useState<bool>(false);
+
+  const onSend = () => {
+    console.log('Do that send thing!');
+    setSentEmail(true);
+    window.open(
+      `mailto:hankins.jamesb@gmail.com?subject=${subject}&body=${body}`
+    );
+  };
+
   return (
     <>
       <div className="flex flex-wrap gap-3">
         <Button
           onPress={() => handleOpen()}
-          // color="primary"
           variant="flat"
           className="bg-tirtiary text-primary opacity-95"
         >
@@ -66,26 +79,39 @@ export default function ContactMe() {
                 Contact Me via Email!
               </ModalHeader>
               <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat
-                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                  aliqua enim laboris do dolor eiusmod.
-                </p>
+                {!sentEmail ? (
+                  <>
+                    <p>
+                      {`Please be aware, this feature will open an email app on
+                      your machine!`}
+                    </p>
+                    <Input
+                      size="md"
+                      type="text"
+                      label="Subject"
+                      value={subject ?? ''}
+                      onChange={(e) => setSubject(e.target.value)}
+                    />
+                    <Textarea
+                      label="Body"
+                      size="md"
+                      placeholder=""
+                      value={body ?? ''}
+                      onChange={(e) => setBody(e.target.value)}
+                    />
+                  </>
+                ) : (
+                  <>
+                    <h1>{`Thanks for reaching out!`}</h1>
+                    <p>{`I'm looking forward to connecting soon.`}</p>
+                  </>
+                )}
               </ModalBody>
               <ModalFooter>
-                <Button color="primary" onPress={onClose}>
-                  <span className="text-tirtiary">Get in touch!</span>
+                <Button color="primary" onPress={onSend}>
+                  <span className="text-tirtiary hover:underline hover:italic hover:text-bold">
+                    Get in touch!
+                  </span>
                 </Button>
               </ModalFooter>
             </>
